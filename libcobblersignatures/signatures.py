8823bd390e05475b88cdeb1ecee8c8a75a479cc7
Enno Gotthold <matrixfueller@gmail.com>
2020-05-22 09:38:05 +0200

    Implement the first methods of signatures.py

--- libcobblersignatures/signatures.py
+++ libcobblersignatures/signatures.py
@@ -1,4 +1,7 @@
 from enum import Enum
+import json
+from typing import List
+import urllib.request
 
 from libcobblersignatures.models.osbreed import OsBreed
 
@@ -17,29 +20,65 @@ class ExportTypes(Enum):
 
 class Signatures:
 
+    _rootkey: str
+    _signaturesjson: json
+    _osbreeds: List[OsBreed]
+
     def __init__(self):
-        print("Test")
+        """
+        If you want to create an empty file just use this constructor. If you want to import one use this and then run
+        an import function.
+        """
         self._rootkey = "breeds"
         self._signaturesjson = None
-
-    def createemptysignatures(self):
-        print("Start from scratch.")
-        self._signaturesjson = OsBreed()
+        self._osbreeds = []
 
     def importsignatures(self, import_type, source):
-        print("Import")
+        """
+        This is the main import function.
+
+        :param import_type: This is one of the four options from the ImportTypes Enum
+        :type import_type: ImportTypes
+        :param source: A string containing the data to be parsed into a json in the end
+        :type source: str
+        """
+        if import_type == ImportTypes.FILE:
+            self._importsignaturesfile(source)
+        elif import_type == ImportTypes.URL:
+            self._importsignaturesurl(source)
+        elif import_type == ImportTypes.STRING:
+            self._importsignaturestring(source)
+        elif import_type == ImportTypes.JSON:
+            self._importsignaturesjson(source)
+        else:
+            raise ValueError("Please use on of the four given options for the source!")
 
     def _importsignaturesfile(self, filepath):
-        print("Import the signatures from a file")
+        filecontent = None
+        with open(filepath) as f:
+            filecontent = f.read()
+        if filecontent is None:
+            raise FileNotFoundError("Filecontent was not correctly read!")
+        self._importsignaturestring(filecontent)
 
     def _importsignaturesurl(self, url):
-        print("Download and then import it.")
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        self._importsignaturestring(data.decode('utf-8'))
 
     def _importsignaturestring(self, jsonstring):
-        print("Import from string")
+        """
+        This decodes a string to a json object. This is done like in the Python Docs described:
+        https://docs.python.org/3/library/json.html#json.JSONDecoder
+
+        :param jsonstring: A str which contains the json.
+        :type jsonstring: str
+        """
+        jsondict = json.loads(jsonstring)
 
     def _importsignaturesjson(self, jsonobject):
         print("Import them from a json object")
+        self._signaturesjson = jsonobject
 
     def exportsignatures(self, export_type, target):
         print("Export")
