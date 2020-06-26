711983d1a530b0c8dc6edead86fd7cbc6a386f31
SchoolGuy <matrixfueller@gmail.com>
2020-06-26 19:55:15 +0200

    Extend the questions and move the submenus to own methods

--- libcobblersignatures/cli.py
+++ libcobblersignatures/cli.py
@@ -79,57 +79,148 @@ edit_menu_questions = [
     }
 ]
 
+edit_add_os_breed = [
+    {
+        "type": "input",
+        "name": "edit_add_os_breed",
+        "message": "What should the name of the new Operating System Breed be?",
+    }
+]
 
-def main_menu():
-    return prompt(main_menu_questions)
+edit_remove_os_breed = [
+    {
+        "type": "list",
+        "name": "edit_remove_os_breed",
+        "message": "What Operating System Breed (and all its versions) do you want to remove?",
+        "choices": [],
+    }
+]
+
+edit_name_os_breed_1 = [
+    {
+        "type": "list",
+        "name": "edit_name_os_breed_1",
+        "message": "Which Operating System Breed do you want to edit?",
+        "choice": [],
+    }
+]
+
+edit_name_os_breed_2 = [
+    {
+        "type": "input",
+        "name": "edit_name_os_breed_2",
+        "message": "What shall be the new name?",
+    }
+]
+
+edit_add_os_version_1 = [
+    {
+        "type": "list",
+        "name": "edit_add_os_version_1",
+        "message": "Under what Operating System Breed shall the new Version be put?",
+        "choice": []
+    }
+]
+
+edit_add_os_version_2 = [
+    {
+        "type": "input",
+        "name": "edit_add_os_version_2",
+        "message": "What shall be the name of the new version?",
+    }
+]
+
+edit_remove_os_version_1 = [
+    {
+        "type": "list",
+        "name": "edit_remove_os_version_1",
+        "message": "In what Operating System Breed is the to be removed OS Version?",
+        "choices": [],
+    }
+]
+
+edit_remove_os_version_2 = [
+    {
+        "type": "list",
+        "name": "edit_remove_os_version_2",
+        "message": "What is the version that you wish to remove?",
+        "choices": [],
+    }
+]
+
+edit_information_os_version = [
+    {
+        "type": "list",
+        "name": "edit_information_os_version",
+        "message": "What key of the Signatures do you want to edit?",
+        "choices": [
+            "signatures",
+            "version_file",
+            "version_file_regex",
+            "kernel_arch",
+            "kernel_arch_regex",
+            "supported_arches",
+            "supported_repo_breeds",
+            "kernel_file",
+            "initrd_file",
+            "isolinux_ok",
+            "default_autoinstall",
+            "kernel_options",
+            "kernel_options_post",
+            "boot_files",
+        ]
+    }
+]
 
 
 def import_menu():
-    return prompt(import_menu_questions)
+    result_import_menu = prompt(import_menu_questions)
+    if result_import_menu["import_menu_source"] in ["URL", "File", "String"]:
+        result_import_menu_2 = prompt(import_menu_questions2)
+        if result_import_menu["import_menu_source"] == "URL":
+            import_type = ImportTypes.URL
+        elif result_import_menu["import_menu_source"] == "File":
+            import_type = ImportTypes.FILE
+        elif result_import_menu["import_menu_source"] == "String":
+            import_type = ImportTypes.STRING
+        else:
+            return
+        os_signatures.importsignatures(import_type, result_import_menu_2["import_menu_signatures"])
 
 
 def export_menu():
-    return prompt(export_menu_questions)
+    result_export_menu = prompt(export_menu_questions)
+    if result_export_menu["export_menu_sources"] in ["String", "File"]:
+        result_export_menu_2 = prompt(export_menu_questions2)
+        if result_export_menu["export_menu_sources"] == "String":
+            os_signatures.exportsignatures(ExportTypes.STRING)
+        elif result_export_menu["export_menu_sources"] == "File":
+            os_signatures.exportsignatures(ExportTypes.FILE, result_export_menu_2["export_menu_signatures"])
+        else:
+            return
 
 
 def edit_menu():
-    return prompt(edit_menu_questions)
+    result_edit_menu = prompt(edit_menu_questions)
+    if result_edit_menu:
+        pass
+    else:
+        return
 
 
 def main():
     main_menu_option_selected = 0
     while main_menu_option_selected != 3:
-        result_main_menu = main_menu()
+        result_main_menu = prompt(main_menu_questions)
         if result_main_menu["main_menu"] == "Import":
             main_menu_option_selected = 0
-            result_import_menu = import_menu()
-            if result_import_menu["import_menu_source"] in ["URL", "File", "String"]:
-                result_import_menu_2 = prompt(import_menu_questions2)
-                if result_import_menu["import_menu_source"] == "URL":
-                    import_type = ImportTypes.URL
-                elif result_import_menu["import_menu_source"] == "File":
-                    import_type = ImportTypes.FILE
-                elif result_import_menu["import_menu_source"] == "String":
-                    import_type = ImportTypes.STRING
-                else:
-                    return
-                os_signatures.importsignatures(import_type, result_import_menu_2["import_menu_signatures"])
+            import_menu()
         elif result_main_menu["main_menu"] == "Export":
             main_menu_option_selected = 1
-            result_export_menu = export_menu()
-            if result_export_menu["export_menu_sources"] in ["String", "File"]:
-                result_export_menu_2 = prompt(export_menu_questions2)
-                if result_export_menu["export_menu_sources"] == "String":
-                    os_signatures.exportsignatures(ExportTypes.STRING)
-                elif result_export_menu["export_menu_sources"] == "File":
-                    os_signatures.exportsignatures(ExportTypes.FILE, result_export_menu_2["export_menu_signatures"])
-                else:
-                    return
+            export_menu()
         elif result_main_menu["main_menu"] == "Edit":
             main_menu_option_selected = 2
-            print("Edit")
-            result_edit_menu = edit_menu()
-            print("Not implemented")
+            edit_menu()
         elif result_main_menu["main_menu"] == "Exit":
             main_menu_option_selected = 3
             print("Any progress which is not exported will be lost. Bye.")
